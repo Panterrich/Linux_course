3771ef5a1d13db0a81426fb71576feae620e0342
Panterrich <durnov.an@phystech.edu>
2021-12-09 20:16:39 +0300

    FIX: remove extra switch in SIGINT

--- src/daemon/main.c
+++ src/daemon/main.c
@@ -21,13 +21,12 @@ int main()
 
     sigset_t sig = {};
     sigmask_configuration(&sig);
-
-    sigset_t sig_stop = sig;
-    sigdelset(&sig_stop, SIGALRM);
-
+    
     alarm(delay);
     int signum = 0;
     siginfo_t info = {};
+
+    int run = 1;
 
     while (1) 
     {   
@@ -151,135 +150,18 @@ int main()
 
             case SIGINT:
             {
-                int ext = 0;
-                syslog(LOG_NOTICE, "daemon POWER[%d] has been stopped", getpid());
-
-                while (1)
+                if (run)
                 {
-                    signum = sigwaitinfo(&sig_stop, &info);
-                    
-                    switch (signum)
-                    {
-                        case SIGUSR1:
-                        {   
-                            if (info.si_int == -1)
-                            {
-                                info.si_int = 0;
-                                syslog(LOG_NOTICE, "controller check this demon");
-                                break;
-                            }
-
-                            delay = (info.si_int) > 0 ? info.si_int : delay;
-                            syslog(LOG_NOTICE, "controller set a new interval: %d", delay);
-                            info.si_int = 0;
-                        }
-                        break;
-                        
-                        case SIGUSR2:
-                        {
-                            if (info.si_int == 0)
-                            {   
-                                memset(src_directory, 0, 1001);
-
-                                if (read(fd_fifo, src_directory, 1000) == -1)
-                                {
-                                    syslog(LOG_ERR, "daemon can't read src directory");
-                                    break;
-                                }
-
-                                DIR* dir = opendir(src_directory);
-
-                                if (!dir)
-                                {
-                                    src_len = 0;
-                                    syslog(LOG_WARNING, "controller send novalid directory path");
-                                    break;
-                                }
-
-                                closedir(dir);
-
-                                if (strcmp(src_directory, dst_directory))
-                                {
-                                    syslog(LOG_WARNING, "source and destination directory are the same, so backup will be ignored");
-                                }
-
-                                src_len = strlen(src_directory);
-                                syslog(LOG_NOTICE, "controller set a new src directory: \"%s\"", src_directory);
-                            }
-                            else
-                            {
-                                memset(dst_directory, 0, 1001);
-
-                                if (read(fd_fifo, dst_directory, 1000) == -1)
-                                {
-                                    syslog(LOG_ERR, "daemon can't read src directory");
-                                    break;
-                                }
-
-                                DIR* dir = opendir(dst_directory);
-
-                                if (!dir)
-                                {
-                                    dst_len = 0;
-                                    syslog(LOG_WARNING, "controller send novalid directory path");
-                                    break;
-                                }
-
-                                closedir(dir);
-
-                                if (strcmp(src_directory, dst_directory))
-                                {
-                                    syslog(LOG_WARNING, "source and destination directory are the same, so backup will be ignored");
-                                }
-
-                                dst_len = strlen(dst_directory);
-                                syslog(LOG_NOTICE, "controller set a new dst directory: \"%s\"", dst_directory);
-                            }
-                        }
-                        break;
-
-                        case SIGHUP:
-                        {
-                            mode = info.si_int;
-
-                            if (mode == MODE_CLASSIC) 
-                            {
-                                syslog(LOG_NOTICE, "controller set CLASSIC mode");
-                            }
-                            else if (mode == MODE_INOTIFY)
-                            {
-                                syslog(LOG_NOTICE, "controller set INOTIFY mode");
-                            }
-                            else
-                            {
-                                mode = MODE_CLASSIC;
-                                syslog(LOG_NOTICE, "controller set UNDEFINED mode, so set default mode - CLASSIC");
-                            }
-
-                            info.si_int = 0;
-                        }
-
-                        case SIGQUIT:
-                        case SIGTERM:
-                        {
-                            syslog(LOG_NOTICE, "terminated");
-                            shutdown(signum);
-                        }
-                        break;
-
-                        case SIGINT:
-                        {
-                            ext = 1;
-                            syslog(LOG_NOTICE, "daemon POWER[%d] has been started", getpid());
-                        }
-
-                        default:
-                        syslog(LOG_WARNING, "undefined signal");
-                        break;
-                    }
-
-                    if (ext == 1) break;
+                    sigdelset(&sig, SIGALRM); 
+                    syslog(LOG_NOTICE, "daemon POWER[%d] has been stopped", getpid());
                 }
+                else
+                {
+                    sigaddset(&sig, SIGALRM); 
+                    syslog(LOG_NOTICE, "daemon POWER[%d] has been started", getpid());
+                }
+                
+                run = !run;  
             }
             break;
 
